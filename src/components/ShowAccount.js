bcfe0df8317611764ca8304795a4bc422a397a5d
zura-japoshvili <japoshvili777@gmail.com>
2022-03-18 21:15:51 +0400

    #4 - update

--- src/components/ShowAccount.js
+++ src/components/ShowAccount.js
@@ -1,27 +1,46 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 export const ShowAccount = ({data}) => {
-  function createContent(){
-  let Content = ''
-    if(data.length !== 0){
-      Content = `
-        <div class='about-cont'>
-          <img src='${data.avatar_url}'>
-          <div>
-            <h5>Account Login: ${data.login}</h5>
-            <h4>Name: ${data.name}</h4>
-            <p>Public repositories: ${data.public_repos}</p>
-          </div>
-        </div>
-      `;
-    }
-    else{
-      Content = '';
-    }
-    return {__html: Content};
+  const [showMoreRepos,setShowMoreRepos] = useState([]);
+
+
+  const showRepos = (data) => {
+    axios.get(data)
+    .then(res => {
+      setShowMoreRepos(res.data)
+    })
+    .catch(err => {
+
+    });
   }
 
+
+  const createContent = () => {
+    if(data.length !== 0){
+      return (
+        <div className='about-cont'>
+          <img src={data.avatar_url} />
+          <div>
+            <h5>Account Login: {data.login}</h5>
+            <h4>Name: {data.name}</h4>
+            <p>Public repositories: {data.public_repos} <span >Show Repos</span></p>
+            <p>Location: {data.location}</p>
+          </div>
+        </div>
+      );
+    }
+    else{
+      return (
+        <></>
+      );
+    }
+  }
+
+  
   return (
-    <div className='acc-cont' dangerouslySetInnerHTML={createContent()}></div>
+    <div className='acc-cont'>
+      {createContent()}
+    </div>
   )
 }
